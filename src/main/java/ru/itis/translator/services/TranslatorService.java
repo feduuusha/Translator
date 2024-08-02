b63bb96c9800e950f93b5f31e4d8f2012cf2b263
Fyodor <voropaevfedor2005@mail.ru>
2024-08-02 10:28:00 +0500

    feat: add custom error page

--- src/main/java/ru/itis/translator/services/TranslatorService.java
+++ src/main/java/ru/itis/translator/services/TranslatorService.java
@@ -2,8 +2,6 @@ package ru.itis.translator.services;
 
 import ru.itis.translator.models.RequestData;
 
-import java.util.List;
-
 public interface TranslatorService {
     String translateWords(RequestData requestData);
 }
